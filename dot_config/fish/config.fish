f189ccba7009d749acf19ea99b3eeff74e6658c6
Nic <286490470+nickerell@users.noreply.github.com>
2026-07-21 16:25:15 -0500

    fix: adds proper paths for homebrew

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -1,2 +1,8 @@
 fish_add_path "$HOME/.local/bin"
+
+fish_add_path "/opt/homebrew/bin"
+fish_add_path "/home/linuxbrew/.linuxbrew/bin"
+fish_add_path "/usr/local/bin"
+
+set -x HOMEBREW_NO_ANALYTICS 1
 set -x EDITOR "nvim"
